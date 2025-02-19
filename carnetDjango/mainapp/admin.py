78b9937adc7389bd3adc13e81e8fdf548576790b
G1lber <martinezfrancogilber@gmail.com>
2025-02-19 10:10:54 -0500

    comit de cambios en admin y modelos
    Se cambio el modelo rol y el modelo Rh,junto a la vista del admin

--- carnetDjango/mainapp/admin.py
+++ carnetDjango/mainapp/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import Ficha
+from .models import Ficha, Rol, UsuarioPersonalizado
 
 # Register your models here.
 admin.site.register(Ficha)
+
+admin.site.register(Rol)
+
+admin.site.register(UsuarioPersonalizado)
+
+    
--- carnetDjango/mainapp/models.py
+++ carnetDjango/mainapp/models.py
@@ -4,47 +4,15 @@ from django.db import models
 # Create your models here.
 
 class Rh(models.Model):
-    A_POSITIVO ='A+'
-    A_NEGATIVO ='A-'
-    B_POSITIVO ='B+'
-    B_NEGATIVO = 'B-'
-    AB_POSITIVO ='AB+'
-    AB_NEGATIVO ='AB-'
-    O_POSITIVO ='O+'
-    O_NEGATIVO = 'O-'
+    nombre_tipo = models.CharField(max_length=3)
+    def __str__(self):
+        return self.nombre_tipo
 
-    NOMBRE_TIPO =[
-        (A_POSITIVO,'A+'),
-        (A_NEGATIVO,'A-'),
-        (B_POSITIVO,'B+'),
-        (B_NEGATIVO, 'B-'),
-        (AB_POSITIVO,'AB+'),
-        (AB_NEGATIVO,'AB-'),
-        (O_POSITIVO,'O+'),
-        (O_NEGATIVO, 'O-'),
-    ]
-    nombre_tipo = models.CharField(max_length=3, choices = NOMBRE_TIPO)
-
-    def rh__(self):
-        return self.nombre_tipo in {self. NOMBRE_TIPO}
 
 class Tipo_doc(models.Model):
-    CEDULA = 'CC'
-    TARJETADEIDENTIDAD = 'TI'
-    LIBRETAMILITAR = 'LM'
-    CEDULAEXTRANJERA = 'CE'
-    REGISTROCIVIL = 'RC'
-
-    NOMBRE_DOC = [
-        (CEDULA ,'CC'),
-        (TARJETADEIDENTIDAD, 'TI'),
-        (LIBRETAMILITAR, 'LM'),
-        (CEDULAEXTRANJERA ,'CE'),
-        (REGISTROCIVIL ,'RC'),
-    ]
-    nombre_doc = models.CharField(max_length=18, choices=NOMBRE_DOC)
-    def tipodoc__(self):
-        return self.nombre_doc in {self. NOMBRE_DOC}
+    nombre_doc = models.CharField(max_length=18)
+    def __str__(self):
+        return self.nombre_doc 
 
 class Ficha(models.Model):
     num_ficha = models.IntegerField(primary_key=True)
@@ -53,6 +21,8 @@ class Ficha(models.Model):
 
 class Rol(models.Model):
     nombre_rol = models.CharField(max_length=100)
+    def __str__(self):
+        return self.nombre_rol
 
 class UsuarioPersonalizado(AbstractUser):
     documento = models.CharField(max_length=20, unique=True, null=True, blank=True)
